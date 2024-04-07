f445d09c93d94eba228e1dbdbffed3ad845c0d8a
Arya Dongre <165511129+aryadongre12@users.noreply.github.com>
2024-04-07 20:50:21 +0530

    Problem 28

--- [0028]-Find-the-Index-of-the-First-Occurrence-in-a-String.java
+++ [0028]-Find-the-Index-of-the-First-Occurrence-in-a-String.java
@@ -1,7 +1,7 @@
 class Solution {
     public int strStr(String haystack, String needle) {
         for(int i=0; i<=haystack.length()-needle.length(); i++){
-            if(needle.equals(haystack.substring(i,i+needle.length()))){
+            if(needle.equals(haystack.substring(i,i+needle.length()))){        // Compare needle to sub-string of haystack
                 return i;
             }
         }
