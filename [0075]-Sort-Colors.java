a5d38a5d57ba24c30d15564876e63c6546503bdd
Arya Dongre <165511129+aryadongre12@users.noreply.github.com>
2024-04-19 15:37:04 +0530

    Problem 75

--- [0075]-Sort-Colors.java
+++ [0075]-Sort-Colors.java
@@ -1,6 +1,6 @@
 class Solution {
     public void sortColors(int[] nums) {
-        for(int i=0; i<=nums.length; i++){
+        for(int i=0; i<nums.length; i++){
             for(int j=i; j<nums.length; j++){
                 if(nums[i] > nums[j]){
                     int a = nums[i];
